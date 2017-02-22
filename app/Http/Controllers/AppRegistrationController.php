13a1e3624adacd15b6f75f4ae52a6b9502901044
tharidnarodrigo <tharindarodrigo@gmail.com>
2017-02-22 09:01:22 +0530

    subscription table updated

--- app/Http/Controllers/AppRegistrationController.php
+++ app/Http/Controllers/AppRegistrationController.php
@@ -52,8 +52,8 @@ class AppRegistrationController extends Controller
         if ($status = 'REGISTERED') {
             if ($subscription->save()) {
 
-                $this->sendServer($status, $subscriberId);
-                return 'True';
+                return $this->sendServer($status, $subscriberId);
+//                return 'True';
             }
 
         }
@@ -80,7 +80,7 @@ class AppRegistrationController extends Controller
         //var_dump($address);
         $arrayField = array(
 
-            "destinationAddresses" => ['tel:94774479260'],
+            "destinationAddresses" => ['tel:'.$subscriberId],
             "message" => $message,
             "applicationId" => APP_ID,
             "password" => APP_PASSWORD
