738890e7e1393c1e148a7f01f9728925af284a2d
tharidnarodrigo <tharindarodrigo@gmail.com>
2017-03-06 19:49:08 +0530

    Login added

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -69,11 +69,11 @@
             @if (Route::has('login'))
                 <div class="top-right links">
                     @if (Auth::check())
-                        <a href="{{ url('/home') }}">Home</a>
+                        <a href="{{ url('/register') }}">Register User</a>
                     @else
                         <a href="{{ url('/login') }}">Login</a>
-                        <a href="{{ url('/register') }}">Register</a>
                     @endif
+
                 </div>
             @endif
 
