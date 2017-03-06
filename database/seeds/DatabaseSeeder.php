37bc06efb22009f7a27c080826998490195ea221
tharidnarodrigo <tharindarodrigo@gmail.com>
2017-03-06 18:12:27 +0530

    Message Seeder added

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,8 +11,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-         $this->call(AscendantSeeder::class);
-         $this->call(MessagesSeeder::class);
-         $this->call(FirstMessageSeeder::class);
+//         $this->call(AscendantSeeder::class);
+//         $this->call(MessagesSeeder::class);
+//         $this->call(FirstMessageSeeder::class);
     }
 }
