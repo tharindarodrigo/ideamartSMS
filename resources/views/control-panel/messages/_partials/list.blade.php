8e08f087e22ddfc9a6627953c88b9cfec309614b
tharidnarodrigo <tharindarodrigo@gmail.com>
2017-03-06 18:22:59 +0530

    Message Seeder added

--- resources/views/control-panel/messages/_partials/list.blade.php
+++ resources/views/control-panel/messages/_partials/list.blade.php
@@ -1,59 +1,57 @@
-<div class="table-responsive">
-    <table class="table table-bordered table-hover">
-        <thead>
+<table class="table table-bordered table-hover">
+    <thead>
+    <tr>
+        <th>#</th>
+        <th>Date</th>
+        <th>Ascendant</th>
+        <th>Message</th>
+        <th>Controls</th>
+    </tr>
+    </thead>
+    <tbody>
+    {{--@if(!empty($messages))--}}
+    @foreach($messages as $message)
         <tr>
-            <th>#</th>
-            <th>Date</th>
-            <th>Ascendant</th>
-            <th>Message</th>
-            <th>Controls</th>
-        </tr>
-        </thead>
-        <tbody>
-        {{--@if(!empty($messages))--}}
-        @foreach($messages as $message)
-            <tr>
-                <td>{!! $message->id !!}</td>
-                <td style="width: 120px;">{!! $message->date !!}</td>
-                <td>{!! $message->ascendant->name!!}</td>
-                <td>{!! $message->message !!}</td>
-                <td style="width: 85px;">
-                    <div class="btn-group">
-                        <a class="btn btn-flat btn-sm btn-primary"
-                           href="{{route('messages.edit',[$message->id])}}"><span
-                                    class="fa fa-edit"></span></a>
-                        <button type="button" data-toggle="modal" data-target="#myModal{{$message->id}}"
-                                class="btn btn-sm btn-danger btn-icon" onSubmit=""><span class=" fa fa-trash"></span>
+            <td>{!! $message->id !!}</td>
+            <td style="width: 120px;">{!! $message->date !!}</td>
+            <td>{!! $message->ascendant->name!!}</td>
+            <td>{!! $message->message !!}</td>
+            <td style="width: 85px;">
+                <div class="btn-group">
+                    <a class="btn btn-flat btn-sm btn-primary"
+                       href="{{route('messages.edit',[$message->id])}}"><span
+                                class="fa fa-edit"></span></a>
+                    <button type="button" data-toggle="modal" data-target="#myModal{{$message->id}}"
+                            class="btn btn-sm btn-danger btn-icon" onSubmit=""><span class=" fa fa-trash"></span>
 
-                        </button>
+                    </button>
 
-                        <div class="modal fade" tabindex="-1" role="dialog" id="myModal{{$message->id}}">
-                            <div class="modal-dialog" role="document">
-                                <div class="modal-content">
-                                    <div class="modal-header">
-                                        <button type="button" class="close" data-dismiss="modal"
-                                                aria-label="Close"><span
-                                                    aria-hidden="true">&times;</span></button>
-                                        <h4 class="modal-title">Delete Message</h4>
-                                    </div>
-                                    <div class="modal-body">
-                                        <p>Are you sure you want to Delete Item ?</p>
-                                    </div>
-                                    {!! Form::open(['route'=>['messages.destroy', $message->id], 'method'=>'delete']) !!}
-                                    <div class="modal-footer">
-                                        <button type="button" class="btn btn-default" data-dismiss="modal">Close
-                                        </button>
-                                        <button class="btn btn-danger">Delete</button>
-                                    </div>
-                                    {!! Form::close() !!}
+                    <div class="modal fade" tabindex="-1" role="dialog" id="myModal{{$message->id}}">
+                        <div class="modal-dialog" role="document">
+                            <div class="modal-content">
+                                <div class="modal-header">
+                                    <button type="button" class="close" data-dismiss="modal"
+                                            aria-label="Close"><span
+                                                aria-hidden="true">&times;</span></button>
+                                    <h4 class="modal-title">Delete Message</h4>
                                 </div>
+                                <div class="modal-body">
+                                    <p>Are you sure you want to Delete Item ?</p>
+                                </div>
+                                {!! Form::open(['route'=>['messages.destroy', $message->id], 'method'=>'delete']) !!}
+                                <div class="modal-footer">
+                                    <button type="button" class="btn btn-default" data-dismiss="modal">Close
+                                    </button>
+                                    <button class="btn btn-danger">Delete</button>
+                                </div>
+                                {!! Form::close() !!}
                             </div>
                         </div>
                     </div>
-                </td>
-            </tr>
-        @endforeach
-        {{--@endif--}}
-        </tbody>
-    </table>
-</div>
+                </div>
+            </td>
+        </tr>
+    @endforeach
+    {{--@endif--}}
+    </tbody>
+</table>
